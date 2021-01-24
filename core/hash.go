447f9dcbaa2e20cacd1531902b8ae33bfaff9592
TheEpicBlock <git.teb@theepicblock.nl>
2021-01-24 19:32:24 +0100

    Add sha1 hashing algorithm

--- core/hash.go
+++ core/hash.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
 	"strings"
@@ -12,6 +13,8 @@ import (
 // GetHashImpl gets an implementation of hash.Hash for the given hash type string
 func GetHashImpl(hashType string) (hash.Hash, error) {
 	switch strings.ToLower(hashType) {
+	case "sha1":
+        return sha1.New(), nil
 	case "sha256":
 		return sha256.New(), nil
 	case "sha512":
