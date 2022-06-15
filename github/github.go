07033023afdee9e9809629a051a79e4e3633651c
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-06-15 14:14:03 +0200

    feat: begin github support

---
+++ github/github.go
@@ -0,0 +1,70 @@
+package github
+
+import (
+	"github.com/packwiz/packwiz/cmd"
+	"github.com/packwiz/packwiz/core"
+	"github.com/spf13/cobra"
+)
+
+var githubCmd = &cobra.Command{
+	Use:     "github",
+	Aliases: []string{"gh"},
+	Short:   "Manage github-based mods",
+}
+
+func init() {
+	cmd.Add(githubCmd)
+	core.Updaters["github"] = ghUpdater{}
+}
+
+type ModReleases struct {
+	URL       string `json:"url"`
+	AssetsURL string `json:"assets_url"`
+	UploadURL string `json:"upload_url"`
+	HTMLURL   string `json:"html_url"`
+	ID        int    `json:"id"`
+	Author    struct {
+		Login             string `json:"login"`
+		ID                int    `json:"id"`
+		NodeID            string `json:"node_id"`
+		AvatarURL         string `json:"avatar_url"`
+		GravatarID        string `json:"gravatar_id"`
+		URL               string `json:"url"`
+		HTMLURL           string `json:"html_url"`
+		FollowersURL      string `json:"followers_url"`
+		FollowingURL      string `json:"following_url"`
+		GistsURL          string `json:"gists_url"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		OrganizationsURL  string `json:"organizations_url"`
+		ReposURL          string `json:"repos_url"`
+		EventsURL         string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"author"`
+	NodeID          string  `json:"node_id"`
+	TagName         string  `json:"tag_name"`
+	TargetCommitish string  `json:"target_commitish"`
+	Name            string  `json:"name"`
+	Draft           bool    `json:"draft"`
+	Prerelease      bool    `json:"prerelease"`
+	CreatedAt       string  `json:"created_at"`
+	PublishedAt     string  `json:"published_at"`
+	Assets          []Asset `json:"assets"`
+	TarballURL      string  `json:"tarball_url"`
+	ZipballURL      string  `json:"zipball_url"`
+	Body            string  `json:"body"`
+	Reactions       struct {
+		URL        string `json:"url"`
+		TotalCount int    `json:"total_count"`
+		Num1       int    `json:"+1"`
+		Num10      int    `json:"-1"`
+		Laugh      int    `json:"laugh"`
+		Hooray     int    `json:"hooray"`
+		Confused   int    `json:"confused"`
+		Heart      int    `json:"heart"`
+		Rocket     int    `json:"rocket"`
+		Eyes       int    `json:"eyes"`
+	} `json:"reactions"`
+}
---
+++ github/updater.go
@@ -0,0 +1,102 @@
+package github
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/packwiz/packwiz/core"
+)
+
+type ghUpdateData struct {
+	ModID            string `mapstructure:"mod-id"` // The slug of the repo but named modId for consistency reasons
+	InstalledVersion string `mapstructure:"version"`
+}
+
+type ghUpdater struct{}
+
+func (u ghUpdater) ParseUpdate(updateUnparsed map[string]interface{}) (interface{}, error) {
+	var updateData ghUpdateData
+	err := mapstructure.Decode(updateUnparsed, &updateData)
+	return updateData, err
+}
+
+type cachedStateStore struct {
+	ModID   string
+	Version ModReleases
+}
+
+func (u ghUpdater) CheckUpdate(mods []core.Mod, mcVersion string, pack core.Pack) ([]core.UpdateCheck, error) {
+	results := make([]core.UpdateCheck, len(mods))
+
+	for i, mod := range mods {
+		rawData, ok := mod.GetParsedUpdateData("modrinth")
+		if !ok {
+			results[i] = core.UpdateCheck{Error: errors.New("couldn't parse mod data")}
+			continue
+		}
+
+		data := rawData.(ghUpdateData)
+
+		newVersion, err := getLatestVersion(data.ModID, pack)
+		if err != nil {
+			results[i] = core.UpdateCheck{Error: fmt.Errorf("failed to get latest version: %v", err)}
+			continue
+		}
+
+		// if newVersion.ID == "" { //There is no version available for this minecraft version or loader.
+		// 	results[i] = core.UpdateCheck{UpdateAvailable: false}
+		// 	continue
+		// }
+
+		if newVersion.TagName == data.InstalledVersion { //The latest version from the site is the same as the installed one
+			results[i] = core.UpdateCheck{UpdateAvailable: false}
+			continue
+		}
+
+		if len(newVersion.Assets) == 0 {
+			results[i] = core.UpdateCheck{Error: errors.New("new version doesn't have any files")}
+			continue
+		}
+
+		newFilename := newVersion.Assets[0].Name
+
+		results[i] = core.UpdateCheck{
+			UpdateAvailable: true,
+			UpdateString:    mod.FileName + " -> " + newFilename,
+			CachedState:     cachedStateStore{data.ModID, newVersion},
+		}
+	}
+
+	return results, nil
+}
+
+func (u ghUpdater) DoUpdate(mods []*core.Mod, cachedState []interface{}) error {
+	for i, mod := range mods {
+		modState := cachedState[i].(cachedStateStore)
+		var version = modState.Version
+
+		var file = version.Assets[0]
+		for _, v := range version.Assets {
+			if strings.HasSuffix(v.Name, ".jar") {
+				file = v
+			}
+		}
+
+		hash, error := file.getSha256()
+		if error != nil || hash == "" {
+			return errors.New("file doesn't have a hash")
+		}
+
+		mod.FileName = file.Name
+		mod.Download = core.ModDownload{
+			URL:        file.BrowserDownloadURL,
+			HashFormat: "sha256",
+			Hash:       hash,
+		}
+		mod.Update["modrinth"]["version"] = version.ID
+	}
+
+	return nil
+}
