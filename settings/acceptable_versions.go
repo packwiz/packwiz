72f9496250fc9a8609b8659b861ac12331827516
mja00 <teamnirvana2@gmail.com>
2023-04-04 23:25:57 -0400

    Support adding and removing specific versions

--- settings/acceptable_versions.go
+++ settings/acceptable_versions.go
@@ -5,6 +5,7 @@ import (
 	"github.com/packwiz/packwiz/cmdshared"
 	"github.com/packwiz/packwiz/core"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/unascribed/FlexVer/go/flexver"
 	"golang.org/x/exp/slices"
 	"os"
@@ -27,48 +28,116 @@ var acceptableVersionsCommand = &cobra.Command{
 			fmt.Printf("Error loading pack: %s\n", err)
 			os.Exit(1)
 		}
-		acceptableVersions := args[0]
-		acceptableVersionsList := strings.Split(acceptableVersions, ",")
-		// Dedupe the list
-		acceptableVersionsDeduped := []string(nil)
-		for i, v := range acceptableVersionsList {
-			if !slices.Contains(acceptableVersionsList[i+1:], v) {
-				acceptableVersionsDeduped = append(acceptableVersionsDeduped, v)
-			}
+		var currentVersions []string
+		// Convert the interface{} to a string slice
+		for _, v := range modpack.Options["acceptable-game-versions"].([]interface{}) {
+			currentVersions = append(currentVersions, v.(string))
 		}
-		// Check if the list of versions is out of order, lowest to highest, and inform the user if it is
-		// Compare the versions one by one to the next one, if the next one is lower, then it's out of order
-		for i, v := range acceptableVersionsDeduped {
-			if flexver.Less(acceptableVersionsDeduped[i+1], v) {
-				fmt.Printf("Warning: Your acceptable versions list is out of order. ")
-				// Give a do you mean example
-				// Clone the list
-				acceptableVersionsDedupedClone := make([]string, len(acceptableVersionsDeduped))
-				copy(acceptableVersionsDedupedClone, acceptableVersionsDeduped)
-				flexver.VersionSlice(acceptableVersionsDedupedClone).Sort()
-				fmt.Printf("Did you mean %s?\n", strings.Join(acceptableVersionsDedupedClone, ", "))
-				if cmdshared.PromptYesNo("Would you like to fix this automatically? [Y/n] ") {
-					// If yes we'll just set the list to the sorted one
-					acceptableVersionsDeduped = acceptableVersionsDedupedClone
-					break
-				} else {
-					// If no we'll just continue
+		// Check our flags to see if we're adding or removing
+		if viper.GetBool("settings.acceptable-versions.add") {
+			// Adding
+			acceptableVersion := args[0]
+			// Check if the version is already in the list
+			if slices.Contains(currentVersions, acceptableVersion) {
+				fmt.Printf("Version %s is already in your acceptable versions list!\n", acceptableVersion)
+				os.Exit(1)
+			}
+			// Add the version to the list and re-sort it
+			currentVersions = append(currentVersions, acceptableVersion)
+			flexver.VersionSlice(currentVersions).Sort()
+			// Set the new list
+			modpack.Options["acceptable-game-versions"] = currentVersions
+			// Save the pack
+			err = modpack.Write()
+			if err != nil {
+				fmt.Printf("Error writing pack: %s\n", err)
+				os.Exit(1)
+			}
+			// Print success message
+			prettyList := strings.Join(currentVersions, ", ")
+			fmt.Printf("Added %s to acceptable versions list, now %s\n", acceptableVersion, prettyList)
+		} else if viper.GetBool("settings.acceptable-versions.remove") {
+			// Removing
+			acceptableVersion := args[0]
+			// Check if the version is in the list
+			if !slices.Contains(currentVersions, acceptableVersion) {
+				fmt.Printf("Version %s is not in your acceptable versions list!\n", acceptableVersion)
+				os.Exit(1)
+			}
+			// Remove the version from the list
+			for i, v := range currentVersions {
+				if v == acceptableVersion {
+					currentVersions = append(currentVersions[:i], currentVersions[i+1:]...)
 					break
 				}
 			}
+			// Sort it just in case it's out of order
+			flexver.VersionSlice(currentVersions).Sort()
+			// Set the new list
+			modpack.Options["acceptable-game-versions"] = currentVersions
+			// Save the pack
+			err = modpack.Write()
+			if err != nil {
+				fmt.Printf("Error writing pack: %s\n", err)
+				os.Exit(1)
+			}
+			// Print success message
+			prettyList := strings.Join(currentVersions, ", ")
+			fmt.Printf("Removed %s from acceptable versions list, now %s\n", acceptableVersion, prettyList)
+		} else {
+			// Overwriting
+			acceptableVersions := args[0]
+			acceptableVersionsList := strings.Split(acceptableVersions, ",")
+			// Dedupe the list
+			acceptableVersionsDeduped := []string(nil)
+			for i, v := range acceptableVersionsList {
+				if !slices.Contains(acceptableVersionsList[i+1:], v) {
+					acceptableVersionsDeduped = append(acceptableVersionsDeduped, v)
+				}
+			}
+			// Check if the list of versions is out of order, lowest to highest, and inform the user if it is
+			// Compare the versions one by one to the next one, if the next one is lower, then it's out of order
+			// If it's only 1 element long, then it's already sorted
+			if len(acceptableVersionsDeduped) > 1 {
+				for i, v := range acceptableVersionsDeduped {
+					if flexver.Less(acceptableVersionsDeduped[i+1], v) {
+						fmt.Printf("Warning: Your acceptable versions list is out of order. ")
+						// Give a do you mean example
+						// Clone the list
+						acceptableVersionsDedupedClone := make([]string, len(acceptableVersionsDeduped))
+						copy(acceptableVersionsDedupedClone, acceptableVersionsDeduped)
+						flexver.VersionSlice(acceptableVersionsDedupedClone).Sort()
+						fmt.Printf("Did you mean %s?\n", strings.Join(acceptableVersionsDedupedClone, ", "))
+						if cmdshared.PromptYesNo("Would you like to fix this automatically? [Y/n] ") {
+							// If yes we'll just set the list to the sorted one
+							acceptableVersionsDeduped = acceptableVersionsDedupedClone
+							break
+						} else {
+							// If no we'll just continue
+							break
+						}
+					}
+				}
+			}
+			modpack.Options["acceptable-game-versions"] = acceptableVersionsDeduped
+			err = modpack.Write()
+			if err != nil {
+				fmt.Printf("Error writing pack: %s\n", err)
+				os.Exit(1)
+			}
+			// Print success message
+			prettyList := strings.Join(acceptableVersionsDeduped, ", ")
+			fmt.Printf("Set acceptable versions to %s\n", prettyList)
 		}
-		modpack.Options["acceptable-game-versions"] = acceptableVersionsDeduped
-		err = modpack.Write()
-		if err != nil {
-			fmt.Printf("Error writing pack: %s\n", err)
-			os.Exit(1)
-		}
-		// Print success message
-		prettyList := strings.Join(acceptableVersionsDeduped, ", ")
-		fmt.Printf("Set acceptable versions to %s\n", prettyList)
 	},
 }
 
 func init() {
 	settingsCmd.AddCommand(acceptableVersionsCommand)
+
+	// Add and remove flags for adding or removing specific versions
+	acceptableVersionsCommand.Flags().BoolP("add", "a", false, "Add a version to the list")
+	acceptableVersionsCommand.Flags().BoolP("remove", "r", false, "Remove a version from the list")
+	_ = viper.BindPFlag("settings.acceptable-versions.add", acceptableVersionsCommand.Flags().Lookup("add"))
+	_ = viper.BindPFlag("settings.acceptable-versions.remove", acceptableVersionsCommand.Flags().Lookup("remove"))
 }
