b6b5c9257dcb8066b17577c069830af203b4d15a
joeyak <joeyak97@gmail.com>
2023-06-15 10:51:46 -0400

    Inline cobra functions

--- cmd/pin.go
+++ cmd/pin.go
@@ -8,62 +8,60 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func createPinRun(pinned bool) func(cmd *cobra.Command, args []string) {
-	return func(cmd *cobra.Command, args []string) {
-		fmt.Println("Loading modpack...")
-		pack, err := core.LoadPack()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		index, err := pack.LoadIndex()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		modPath, ok := index.FindMod(args[0])
-		if !ok {
-			fmt.Println("Can't find this file; please ensure you have run packwiz refresh and use the name of the .pw.toml file (defaults to the project slug)")
-			os.Exit(1)
-		}
-		modData, err := core.LoadMod(modPath)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		modData.Pin = pinned
-		format, hash, err := modData.Write()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		err = index.RefreshFileWithHash(modPath, format, hash, true)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		err = index.Write()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		err = pack.UpdateIndexHash()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		err = pack.Write()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		message := "pinned"
-		if !pinned {
-			message = "unpinned"
-		}
-		fmt.Printf("%s %s successfully!\n", args[0], message)
+func pinMod(cmd *cobra.Command, args []string, pinned bool) {
+	fmt.Println("Loading modpack...")
+	pack, err := core.LoadPack()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	index, err := pack.LoadIndex()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	modPath, ok := index.FindMod(args[0])
+	if !ok {
+		fmt.Println("Can't find this file; please ensure you have run packwiz refresh and use the name of the .pw.toml file (defaults to the project slug)")
+		os.Exit(1)
+	}
+	modData, err := core.LoadMod(modPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	modData.Pin = pinned
+	format, hash, err := modData.Write()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = index.RefreshFileWithHash(modPath, format, hash, true)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = index.Write()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = pack.UpdateIndexHash()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = pack.Write()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	message := "pinned"
+	if !pinned {
+		message = "unpinned"
+	}
+	fmt.Printf("%s %s successfully!\n", args[0], message)
 }
 
 // pinCmd represents the pin command
@@ -71,7 +69,9 @@ var pinCmd = &cobra.Command{
 	Use:   "pin",
 	Short: "Pin a mod to the current version",
 	Args:  cobra.ExactArgs(1),
-	Run:   createPinRun(true),
+	Run: func(cmd *cobra.Command, args []string) {
+		pinMod(cmd, args, true)
+	},
 }
 
 // unpinCmd represents the unpin command
@@ -79,7 +79,9 @@ var unpinCmd = &cobra.Command{
 	Use:   "unpin",
 	Short: "Unpin a mod from the current version",
 	Args:  cobra.ExactArgs(1),
-	Run:   createPinRun(false),
+	Run: func(cmd *cobra.Command, args []string) {
+		pinMod(cmd, args, false)
+	},
 }
 
 func init() {
