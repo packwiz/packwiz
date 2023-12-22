0c8beeac2fe7a771bd1ddae81d6737af470599de
Whovian9369 <Whovian9369@gmail.com>
2023-12-22 17:17:51 -0500

    Fix outputting `vendorSha256` to `nix/vendor-sha256` (#266)
    * Fix outputting `vendorSha256` to `nix/vendor-sha256`
    
    * Add `nix/vendor-sha256` to make tools like `nix run` happy.

--- nix/prefetcher.nix
+++ nix/prefetcher.nix
@@ -3,7 +3,13 @@
   pkgs ? import <nixpkgs> {},
 }:
 pkgs.callPackage (import ./.) {
-  buildGoModule = pkgs.buildGo118Module;
+  ## Keeping `buildGoModule` commented out in case it's needed in the future.
+  ## As of writing, `buildGo121Module` is currently used as the default for
+  ## `buildGoModule` in nixpkgs. `buildGo118Module` seems deprecated and
+  ## entirely removed from nixpkgs, so manually setting that is likely to cause
+  ## issues in the future.
+
+  # buildGoModule = pkgs.buildGo118Module;
   vendorSha256 = sha256;
 }
 // {
