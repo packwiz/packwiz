9a31fc5ddfdbedade8ff8a4b6c9095e196a1c7a6
cswimr <cswimr@csw.im>
2026-04-21 14:34:31 -0400

    Nix Flake cleanup work
    - Moved from [`nix-prefetch`](https://github.com/msteen/nix-prefetch) to
    [`nix-update`](https://github.com/Mic92/nix-update).
    - Fixed broken `vendorHash` fetching.
      - Vendor hashes are now stored in the package definition itself,
    instead of in a separate file. `nix-update` will update the hash in the
    package definition directly in CI, so the separate file isn't necessary.
      - `nix-update` additionally uses the flake entry point, meaning it
    works without the `prefetcher.nix` file, so that has also been removed.
    - Changed default formatters from `alejandra` to the standard `nixfmt`.
    - Reformatted nix files with the changed formatter.
    
    I have tested this locally by updating a random dependency in `go.mod`,
    and can confirm that `nix-update` successfully generates a new
    `vendorHash` that can be used to build the package.
    
    Closes #387, supersedes #388.

--- nix/default.nix
+++ nix/default.nix
@@ -1,38 +1,34 @@
-let
-  # Import nixpkgs if needed
-  pkgs = import <nixpkgs> {};
-in
-  {
-    lib ? pkgs.lib,
-    buildGoModule ? pkgs.buildGoModule,
-    fetchFromGitHub ? pkgs.fetchFromGitHub,
-    installShellFiles ? pkgs.installShellFiles,
-    # version and vendorHash should be specified by the caller
-    version ? "latest",
-    vendorHash,
-  }:
-    buildGoModule rec {
-      pname = "packwiz";
-      inherit version vendorHash;
+{
+  lib ? lib,
+  buildGoModule ? buildGoModule,
+  installShellFiles ? installShellFiles,
+  # version should be specified by the caller
+  version ? "latest",
+}:
+buildGoModule {
+  pname = "packwiz";
+  inherit version;
 
-      src = ./..;
+  src = ./..;
 
-      nativeBuildInputs = [
-        installShellFiles
-      ];
+  vendorHash = "sha256-ChUE4hWl+UyPpbzK0GbJTD0AoBCogI7qGstga4+WujI=";
 
-      # Install shell completions
-      postInstall = ''
-        installShellCompletion --cmd packwiz \
-          --bash <($out/bin/packwiz completion bash) \
-          --fish <($out/bin/packwiz completion fish) \
-          --zsh <($out/bin/packwiz completion zsh)
-      '';
+  nativeBuildInputs = [
+    installShellFiles
+  ];
 
-      meta = with lib; {
-        description = "A command line tool for editing and distributing Minecraft modpacks, using a git-friendly TOML format";
-        homepage = "https://packwiz.infra.link/";
-        license = licenses.mit;
-        mainProgram = "packwiz";
-      };
-    }
+  # Install shell completions
+  postInstall = ''
+    installShellCompletion --cmd packwiz \
+      --bash <($out/bin/packwiz completion bash) \
+      --fish <($out/bin/packwiz completion fish) \
+      --zsh <($out/bin/packwiz completion zsh)
+  '';
+
+  meta = with lib; {
+    description = "A command line tool for editing and distributing Minecraft modpacks, using a git-friendly TOML format";
+    homepage = "https://packwiz.infra.link/";
+    license = licenses.mit;
+    mainProgram = "packwiz";
+  };
+}
